94b523ecef0336d6ca8ca9c4fa230617bc72eb23
Anthony Balmeo <anthony.balmeo@dollarshaveclub.com>
2017-10-11 16:35:56 -0500

    Test home-data

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
 <script>
+import homeData from './static/fixtures/home-data.json'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      homeData
+    }
+  }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" src="./styles/app.scss">
 </style>
---
+++ src/components/Home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="home__container">
+      <!-- <h1>{{ homeData.title }}</h1> -->
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+}
+</script>
+
+<style lang="scss" src="../../styles/app.scss">
+</style>
